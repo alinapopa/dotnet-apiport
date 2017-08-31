38d12e93071df408325e5c0cf92072bf665c4d11
Alina Popa <alpopa@microsoft.com>
2017-08-31 14:48:48 -0700

    Move NuGetPackageId into a separate file, ass Equals and GetHashCode to NuGetPackageInfo

--- src/Microsoft.Fx.Portability/ObjectModel/NuGetPackageInfo.cs
+++ src/Microsoft.Fx.Portability/ObjectModel/NuGetPackageInfo.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.Versioning;
 
@@ -9,22 +10,39 @@ namespace Microsoft.Fx.Portability.ObjectModel
 {
     public class NuGetPackageInfo
     {
+        private bool _hashComputed;
+        private int _hashCode;
+
         public string AssemblyInfo { get; private set; }
         public FrameworkName Target { get; private set; }
-        public IEnumerable<NuGetPackageId> SupportedPackages { get; private set; }
+        public ImmutableList<NuGetPackageId> SupportedPackages { get; private set; }
 
-        public NuGetPackageInfo( string assemblyInfo, FrameworkName target, IEnumerable<NuGetPackageId> supportedPackages)
+        public NuGetPackageInfo(string assemblyInfo, FrameworkName target, IEnumerable<NuGetPackageId> supportedPackages)
         {
             AssemblyInfo = assemblyInfo;
             Target = target;
-            SupportedPackages = supportedPackages != null? supportedPackages.ToList(): Enumerable.Empty<NuGetPackageId>();
+            var packages = supportedPackages != null ? supportedPackages : Enumerable.Empty<NuGetPackageId>();
+            SupportedPackages = packages.ToImmutableList();
         }
-    }
 
-    public class NuGetPackageId
-    {
-        public string PackageId { get; set; }
-        public string Version { get; set; }
-        public string Hyperlink { get; set; }
+        public override bool Equals(object obj)
+        {
+            NuGetPackageInfo other = obj as NuGetPackageInfo;
+            if (other == null)
+                return false;
+
+            return string.Equals(other.AssemblyInfo, AssemblyInfo, System.StringComparison.Ordinal)
+                && Target.Equals(other.Target);
+        }
+
+        public override int GetHashCode()
+        {
+            if (!_hashComputed)
+            {
+                _hashCode = (AssemblyInfo ?? string.Empty).GetHashCode() + Target.GetHashCode();
+                _hashComputed = true;
+            }
+            return _hashCode;
+        }
     }
 }
